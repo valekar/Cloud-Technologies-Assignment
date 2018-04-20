6f9f5be714f6f432f53f19a3c161056903de0655
valekar <srinivas.valekar@gmail.com>
2018-04-20 18:51:39 +0800

    added client code and first screen

--- client/src/app/pages/models/URLS.ts
+++ client/src/app/pages/models/URLS.ts
@@ -4,12 +4,8 @@ export class URLS{
     //public static cloud_provider = "http://cloud-env.vmfc4stcsn.eu-west-1.elasticbeanstalk.com/";
     //dev
     public static cloud_provider = "";
-    
-  
-
     public static GET_CATEGORIES = URLS.cloud_provider +"/api/categories/";
     public static GET_SUB_CATEGORIES = URLS.cloud_provider + "/api/sub/categories/";
     public static GET_COUNTRIES = URLS.cloud_provider + "/api/countries/";
-
     public static GET_TOP_PROJECTS = URLS.cloud_provider + "/api/projects/";
 }
